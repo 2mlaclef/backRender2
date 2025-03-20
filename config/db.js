b7fc80de13255a4760095b91d76dc5cec8df1ef3
unknown <2m.laclef@gmail.com>
2025-03-20 11:11:28 +0000

    ajout

--- config/db.js
+++ config/db.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      process.env.MONGO_URI ||
+        "mongodb+srv://2mlaclef:0987654321a@cluster0.x6u02.mongodb.net/?retryWrites=true&w=majority"
+    );
     console.log("✅ MongoDB connecté avec succès !");
   } catch (error) {
-    console.error("❌ Erreur de connexion à MongoDB :", error);
+    console.error("❌ Erreur de connexion à MongoDB :", error.message);
     process.exit(1);
   }
 };
